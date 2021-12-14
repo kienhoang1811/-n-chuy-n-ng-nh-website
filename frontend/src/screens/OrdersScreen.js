29822b7612225343ec61a8ea7a1b046dbbe8a6d9
kienhoang1811 <hoangtrongkien18112000hiie@gmail.com>
2021-12-14 22:33:21 +0700

    14-12-21

--- frontend/src/screens/OrdersScreen.js
+++ frontend/src/screens/OrdersScreen.js
@@ -6,11 +6,14 @@ import { listOrders, deleteOrder } from '../actions/orderActions';
 function OrdersScreen(props) {
   const orderList = useSelector(state => state.orderList);
   const { loading, orders, error } = orderList;
-
+  
   const orderDelete = useSelector(state => state.orderDelete);
   const { loading: loadingDelete, success: successDelete, error: errorDelete } = orderDelete;
 
   const dispatch = useDispatch();
+  useEffect(()=> {
+    console.log(orders);
+  },[orders])
 
   useEffect(() => {
     dispatch(listOrders());
@@ -40,26 +43,36 @@ function OrdersScreen(props) {
               <th>PAID</th>
               <th>PAID AT</th>
               <th>DELIVERED</th>
-              <th>DELIVERED AT</th>
+              {/* <th>DELIVERED AT</th> */}
               <th>ACTIONS</th>
             </tr>
           </thead>
           <tbody>
-            {orders.map(order => (<tr key={order._id}>
+            {orders.map(order => {
+              const time = new Date(order.createdAt);
+              const day = time.getDay()
+              const month = time.getMonth()
+              const year = time.getFullYear()
+              const hour = time.getHours()
+              const minute = time.getMinutes()
+              const second = time.getSeconds()
+              
+              return (<tr key={order._id}>
+              
               <td>{order._id}</td>
-              <td>{order.createdAt}</td>
+              <td>{`${day}/${month}/${year} ${hour}:${minute}:${second}`}</td>
               <td>{order.totalPrice}</td>
               <td>{order.user.name}</td>
               <td>{order.isPaid.toString()}</td>
               <td>{order.paidAt}</td>
               <td>{order.isDelivered.toString()}</td>
-              <td>{order.deliveredAt}</td>
+              {/* <td>{order.deliveredAt}</td> */}
               <td>
                 <Link to={"/order/" + order._id} className="button secondary" >Details</Link>
                 {' '}
                 <button type="button" onClick={() => deleteHandler(order)} className="button secondary">Delete</button>
               </td>
-            </tr>))}
+            </tr>)})}
           </tbody>
         </table>
 
